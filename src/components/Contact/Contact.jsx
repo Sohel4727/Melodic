9ee3a4f0e7873846074fcb3c23c25dd0a9248760
Sayyed Sohel Ahmed <sayyedsohel73976@gmail.com>
2024-07-05 00:21:40 +0530

    develop footer and contact page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
-
+import React from "react";
+import contactImg from "../../assets/contact.png";
+import "./Contact.css";
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div className="main_contact_container">
+      <div className="get_in_touch_container">
+        <h1 className="get_in_touch_heading">Get in Touch With Us</h1>
+      </div>
+      <div className="contact_section">
+        <div className="contact_form">
+            <div className="contact_name_section">
+          <span>
+            <label className="contact_lable">Name</label>
+            <div className="input_background">
+              <input className="contact_input"  placeholder='John Doe' type="text" />
+            </div>
+          </span>
+          <span className="contact_phone">
+            <label className="contact_lable">Phone</label>
+            <div className="input_background">
+              <input className="contact_input" placeholder='1234567890' type="number" />
+            </div>
+          </span>
+            </div>
+           
+            <div className="contact_email">
+            <label className="contact_lable">Email</label>
+            <div className="input_background">
+              <input className="contact_input" placeholder='dummy@gmail.com' type="email" />
+            </div>
+          </div>
+            <div className="contact_email">
+            <label className="contact_lable">Want to say something?</label>
+            <div className="input_background">
+              <textarea className="contact_textarea" placeholder='Type here...' type="text" />
+            </div>
+            <button className="send_button">SEND</button>
+          </div>
+        </div>
+        <img src={contactImg} width={400} alt="contact" />
+      </div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,25 +1,48 @@
 import React from "react";
 import logo from "../../assets/logo.png";
+import facebook from "../../assets/Social/facebook.png";
 import "./Footer.css";
 const Footer = () => {
   return (
     <footer className="footer_main_container">
+        <section className="content_section">
       <div className="logo_container">
         <img src={logo} alt="logo" />
-        <h1 className="footer_heading">Melodic Byte</h1>
+        <p className="footer_heading">Melodic <b>Byte</b></p>
       </div>
-      <p>
+      <p className="footer_description">
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet vero
         provident facere blanditiis omnis architecto quae nisi dolorem tempora
         ab!
       </p>
-      <p>social icons</p>
+      <div className="social_icon_container">
+        <img src={facebook} alt="facebook" />
+        <img src={facebook} alt="linkedIn" />
+        <img src={facebook} alt="social" />
+        <img src={facebook} alt="instagram" />
+      </div>
       <div className="pages_links">
         <p>About us</p>
         <p>Portfolio</p>
         <p>Carrers</p>
         <p>Privacy Policy</p>
         <p>Terms of Use</p>
+      </div>
+        </section>
+      <div className="contact_details_container">
+        <div className="have_a_question">
+        <span>
+            <p className="contact_title">Have a Questions?</p>
+            <p>310-437-2766</p>
+        </span>
+        <span>
+            <p className="contact_title">Contact us at</p>
+            <p>unreal@outlook.com</p>
+        </span>
+        </div>
+        <div className="year_container">
+            <p>c 2000-2001, All Right Reserved</p>
+        </div>
       </div>
     </footer>
   );
